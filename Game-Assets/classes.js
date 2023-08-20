8258472a778fe22a37a475d25fef5bcf40dce3fc
vhuang325 <vhuang325@gmail.com>
2023-08-20 02:04:28 -0400

    added more to battle sequence

--- Game-Assets/classes.js
+++ Game-Assets/classes.js
@@ -1,10 +1,13 @@
 class Sprite {
   constructor({
     position,
+    velocity,
     image,
     frames = { max: 1, hold: 10 },
     sprites,
     animate = false,
+    isEnemy = false,
+    rotation = 0,
   }) {
     this.position = position;
     this.image = image;
@@ -16,9 +19,24 @@ class Sprite {
     };
     this.animate = animate;
     this.sprites = sprites;
+    this.opacity = 1;
+    this.health = 100;
+    this.isEnemy = isEnemy;
+    this.rotation = rotation;
   }
 
   draw() {
+    context.save();
+    context.translate(
+      this.position.x + this.width / 2,
+      this.position.y + this.height / 2
+    );
+    context.rotate(this.rotation);
+    context.translate(
+      -this.position.x - this.width / 2,
+      -this.position.y - this.height / 2
+    );
+    context.globalAlpha = this.opacity;
     context.drawImage(
       this.image,
       this.frames.val * this.width,
@@ -30,6 +48,7 @@ class Sprite {
       this.image.width / this.frames.max,
       this.image.height
     );
+    context.restore();
 
     if (!this.animate) return;
 
@@ -40,6 +59,98 @@ class Sprite {
     if (this.frames.elapsed % this.frames.hold === 0) {
       if (this.frames.val < this.frames.max - 1) this.frames.val++;
       else this.frames.val = 0;
+    }
+  }
+
+  attack({ attack, recipient, renderedSprites }) {
+    let healthBar = "#enemyHealthBar";
+    if (this.isEnemy) healthBar = "#playerHealthBar";
+
+    let rotation = 1;
+    if (this.isEnemy) rotation = -2.2;
+
+    this.health -= attack.damage;
+
+    switch (attack.name) {
+      case "Fireball":
+        const fireballImage = new Image();
+        fireballImage.src = "./images/fireball.png";
+        const fireball = new Sprite({
+          position: {
+            x: this.position.x,
+            y: this.position.y,
+          },
+          image: fireballImage,
+          frames: {
+            max: 4,
+            hold: 10,
+          },
+          animate: true,
+          rotation,
+        });
+
+        renderedSprites.splice(1, 0, fireball);
+
+        gsap.to(fireball.position, {
+          x: recipient.position.x,
+          y: recipient.position.y,
+          onComplete: () => {
+            gsap.to(healthBar, {
+              width: this.health + "%",
+            });
+
+            gsap.to(recipient.position, {
+              x: recipient.position.x + 10,
+              yoyo: true,
+              repeat: 5,
+              duration: 0.08,
+            });
+            gsap.to(recipient, {
+              opacity: 0,
+              repeat: 5,
+              yoyo: true,
+              duration: 0.08,
+            });
+            renderedSprites.splice(1, 1);
+          },
+        });
+        break;
+      case "Tackle":
+        const tl = gsap.timeline();
+
+        let movementDistance = 20;
+        if (this.isEnemy) movementDistance = -20;
+
+        tl.to(this.position, {
+          x: this.position.x - movementDistance,
+        })
+          .to(this.position, {
+            x: this.position.x + movementDistance * 2,
+            duration: 0.1,
+            onComplete: () => {
+              //Enemy gets hit
+              gsap.to(healthBar, {
+                width: this.health - attack.damage + "%",
+              });
+
+              gsap.to(recipient.position, {
+                x: recipient.position.x + 10,
+                yoyo: true,
+                repeat: 5,
+                duration: 0.08,
+              });
+              gsap.to(recipient, {
+                opacity: 0,
+                repeat: 5,
+                yoyo: true,
+                duration: 0.08,
+              });
+            },
+          })
+          .to(this.position, {
+            x: this.position.x,
+          });
+        break;
     }
   }
 }
--- Game-Assets/index.js
+++ Game-Assets/index.js
@@ -347,6 +347,7 @@ const draggle = new Sprite({
     hold: 25,
   },
   animate: true,
+  isEnemy: true,
 });
 
 const embyImage = new Image();
@@ -364,14 +365,30 @@ const emby = new Sprite({
   animate: true,
 });
 
+const renderedSprites = [draggle, emby];
 function animateBattle() {
   window.requestAnimationFrame(animateBattle);
   battleBackground.draw();
-  draggle.draw();
-  emby.draw();
+
+  renderedSprites.forEach((sprite) => {
+    sprite.draw();
+  });
 }
 
+// animate
 animateBattle();
+
+// our event listeners for our buttons (attack)
+document.querySelectorAll("button").forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const selectedAttack = attacks[e.currentTarget.innerHTML];
+    emby.attack({
+      attack: selectedAttack,
+      recipient: draggle,
+      renderedSprites,
+    });
+  });
+});
 
 let lastKey = "";
 window.addEventListener("keydown", (event) => {
